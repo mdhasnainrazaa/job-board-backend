c453cf9a4486f85c8c0b2d84062f1b857f25cbc4
Md Hasnain Raza <mdhasnainraza463@gmail.com>
2025-07-02 13:10:13 +0530

    Module 3 till exceptional handling

---
+++ common/src/main/java/com/xcelerateit/common/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.xcelerateit.common;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
